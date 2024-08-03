9ab507a335095a43a525b38174aa1649b9b0082e
VladimirFilim <boterfly6@gmail.com>
2024-08-03 14:19:51 +0400

    adeed private final field in ProductController

---
+++ src/main/java/com/study/sqch7ex1/Main.java
@@ -0,0 +1,7 @@
+package com.study.sqch7ex1;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/study/sqch7ex1/controllers/ProductController.java
+++ src/main/java/com/study/sqch7ex1/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProductController {
+
     private final ProductService productService;
 
     public ProductController(ProductService productService) {
